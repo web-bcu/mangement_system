8077012c4c1abb45a43b2821d4462cfdd750418b
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-12-26 13:56:29 +0700

    [UPDATE]

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-export const TASK_API_URL = "http://localhost:8070/api/v1/tasks";
+export const TASK_API_URL = "http://localhost:8080/api/v1/tasks";
 
-export const FINANCE_API_URL = "http://localhost:8050/api/v1/finance";
+export const FINANCE_API_URL = "http://localhost:8080/api/v1/finance";
